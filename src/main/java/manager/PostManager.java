55b5f6bee941961dc5e3241ad2fa4b2524234934
Nikoly <turnik3580@gmail.com>
2020-09-18 17:41:00 +0300

    add PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,14 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, int offset, int ownerOnly, int count){
+    return posts;
+}
+
+    public void delete(int postId, int ownerId) {
+    }
+}
